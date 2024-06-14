dc6776e4410e9f2bfaf97c84609a3cedc0446c0d
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2024-06-14 17:25:05 +0900

    fix: missing file

--- update_gpg.sh
+++ update_gpg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function main(){
-    local -r VERSION=`../get_ubuntu_version.sh`
+    local -r VERSION=`./get_ubuntu_version.sh`
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/cuda-keyring_1.0-1_all.deb
     sudo dpkg -i cuda-keyring_1.0-1_all.deb
     sudo apt update
